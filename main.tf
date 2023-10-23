03734a226e232df03ce61359de1268496c945655
nizarakbarm <nizar@domainesia.com>
2023-10-23 14:40:42 +0700

    feat: :sparkles: Add kubernetes provider configuration by using data source of aws_eks_cluster

--- main.tf
+++ main.tf
@@ -4,3 +4,13 @@ data "aws_eks_cluster" "default" {
 output "check_cluster_name" {
   value = data.aws_eks_cluster.default.name
 }
+provider "kubernetes" {
+  host = data.aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args = [ "eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.name ]
+    command = "aws"
+  }
+}
